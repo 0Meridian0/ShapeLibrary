21df11f4510341ad897391f593de27db3f70e389
meridian <boss.sasha18@mail.ru>
2024-09-11 14:46:42 +0300

    Добавил проект с использованием сборки.

---
+++ TestOfUsingShapeLibrary/Program.cs
@@ -0,0 +1,11 @@
+﻿using ShapeLibrary;
+
+IShape shape = new Circle(7);
+//IShape shape = new Cicrle(-7);
+shape = new Triangle(3, 4, 6);
+
+var area = shape.CalculateShapeArea();
+Console.WriteLine(area);
+
+var triangle = new Triangle(3, 4, 5);
+Console.WriteLine($"Является ли треугольник прямоугольным: {triangle.IsRightTriangle()}");
