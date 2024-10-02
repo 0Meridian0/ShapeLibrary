43064627e588539663e850703af91cd975d51f5d
meridian <boss.sasha18@mail.ru>
2024-10-02 11:39:20 +0300

    Изменил сравнение чисел с плавающей точкой. Закрыл публичные сеттеры свойств.

--- ShapeLibrary/Shapes.cs
+++ ShapeLibrary/Shapes.cs
@@ -7,7 +7,7 @@
 
     public class Circle : IShape
     {
-        public double Radius { get; set; }
+        public double Radius { get; private set; }
 
         public Circle(double radius)
         {
@@ -27,9 +27,9 @@
 
     public class Triangle : IShape
     {
-        public double A { get; set; }
-        public double B { get; set; }
-        public double C { get; set; }
+        public double A { get; private set; }
+        public double B { get; private set; }
+        public double C { get; private set; }
 
         public Triangle(double a, double b, double c)
         {
@@ -37,7 +37,7 @@
             {
                 throw new ArgumentException("Стороны треугольника не могут быть отрицательными", nameof(a));
             }
-            if (a + b <= c || a + c <= b || b + c <= a)
+            if ((a + b).CompareTo(c) == 0 || (a + c).CompareTo(b) == 0 || (b + c).CompareTo(a) == 0)
             {
                 throw new ArgumentException("Стороны треугольника не удовлетворяют неравенству треугольника", nameof(a));
             }
@@ -49,13 +49,15 @@
 
         public double CalculateShapeArea()
         {
-            var s = (A + B + C) / 2;
-            return Math.Sqrt(s * (s - A) * (s - B) * (s - C));
+            var halfPerimeter = (A + B + C) / 2;
+            return Math.Sqrt(halfPerimeter * (halfPerimeter - A) * (halfPerimeter - B) * (halfPerimeter - C));
         }
 
         public bool IsRightTriangle()
         {
-            return (A * A + B * B == C * C) || (A * A + C * C == B * B) || (C * C + B * B == A * A);
+            return ((A * A + B * B).CompareTo(C * C) == 0 ||
+                    (A * A + C * C).CompareTo(B * B) == 0 ||
+                    (C * C + B * B).CompareTo(A * A) == 0);
         }
     }
 }
